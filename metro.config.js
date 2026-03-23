641e6fb300079a463d5d30e9fe05961b3a1dfcc1
mixailksk-cyber <mixailksk@gmail.com>
2026-03-23 17:32:56 +0500

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,6 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
+const defaultConfig = getDefaultConfig(__dirname);
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(defaultConfig, config);
