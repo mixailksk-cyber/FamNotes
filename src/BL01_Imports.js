6bb7facf63528bd4e5c034d7091c06058fa4f87e
mixailksk-cyber <mixailksk@gmail.com>
2026-03-23 22:49:51 +0500

    Create BL01_Imports.js

---
+++ src/BL01_Imports.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+  Alert,
+  ScrollView,
+  StatusBar,
+  Dimensions,
+  Platform,
+  KeyboardAvoidingView
+} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import DocumentPicker from 'react-native-document-picker';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+export {
+  React,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+  Alert,
+  ScrollView,
+  StatusBar,
+  Dimensions,
+  Platform,
+  KeyboardAvoidingView,
+  AsyncStorage,
+  DocumentPicker,
+  Icon,
+  useSafeAreaInsets
+};
